e65957875a79a7a4eec2c3208c266917555f794b
ikkai-r <rm.sales92@gmail.com>
2023-10-04 14:18:11 +0800

    made layout

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT;
 const bodyParser = require(`body-parser`);
 const routes = require(`./routes/routes.js`);
 const hbs = require(`hbs`);
 
-app.set("view engine", "hbs");
-app.set("views", "./views");
-hbs.registerPartials(__dirname + `/views/partials`);
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(`public`));
 app.use(`/`, routes);
+
+//app.engine('handlebars', exphbs.engine({extname: '.hbs', defaultLayout: 'main'}));
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+hbs.registerPartials(__dirname + `/views/partials`);
 
 app.listen(PORT, () => {
     console.log("Server listening. Port: " + PORT);
