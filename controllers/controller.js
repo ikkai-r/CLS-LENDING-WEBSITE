8d2a25eed071edff4fcb30afcead143278ea5b0e
Rica Sales <92283086+ikkai-r@users.noreply.github.com>
2023-10-04 09:45:21 +0800

    setup

---
+++ controllers/controller.js
@@ -0,0 +1,10 @@
+const controller = {
+
+    getIndex: function(req, res) {
+        // your code here
+        res.render('index'); // This is to load the page initially
+    }
+    
+}
+
+module.exports = controller;
