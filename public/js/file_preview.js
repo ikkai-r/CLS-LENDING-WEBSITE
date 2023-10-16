9176f72327ce1eea50fc75acb449d0692f405dc0
Batwoper <marubeni293@gmail.com>
2023-10-17 01:13:51 +0800

    register pt2
    i was able to finish the image preview thing but for some reason its not connecting (js <-> hbs)
    i couldnt do the google api thing even with the tutorials

--- public/js/file_preview.js
+++ public/js/file_preview.js
@@ -1,23 +1,22 @@
-function handleFileUpload(inputElement, previewElement) {
-    inputElement.addEventListener('change', function() {
-        const file = this.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = function() {
-                const img = document.createElement('img');
-                img.src = reader.result;
-                previewElement.innerHTML = '';
-                previewElement.appendChild(img);
-            }
-            reader.readAsDataURL(file);
-        }
-    });
-}
+const inpFile = document.getElementById("inpFile");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
 
-const idImageInput = document.getElementById('id_image');
-const idImagePreview = document.getElementById('id_image_preview');
-handleFileUpload(idImageInput, idImagePreview);
+inpFile.addEventListener("change", function() {
+    const file = this.isDefaultNamespace[0];
 
-const signatureImageInput = document.getElementById('signature_image');
-const signatureImagePreview = document.getElementById('signature_image_preview');
-handleFileUpload(signatureImageInput, signatureImagePreview);
+    if (file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function() {
+            console.log(this);
+            previewImage.setAttribute("src", this.result);
+        });
+
+        reader.readAsDataURL(file);
+    }
+});
