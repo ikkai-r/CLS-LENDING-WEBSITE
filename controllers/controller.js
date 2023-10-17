e9776b9f6e729b7bf36884201881fc39f75793e1
ikkai-r <rm.sales92@gmail.com>
2023-10-17 18:56:11 +0800

    doing client_dashboard

--- controllers/controller.js
+++ controllers/controller.js
@@ -11,7 +11,7 @@ const controller = {
         res.render('dashboard', {layout: 'layouts/main'});
     },
     getClientDashboard: function (req, res) {
-        res.render('client_dashboard', {layout: 'layouts/main'});
+        res.render('client_dashboard', {layout: 'layouts/main_no'});
     },
     getLogin: function (req, res) {
         res.render('login', {layout: 'layouts/main_no', css: '/css/login2.css'});
