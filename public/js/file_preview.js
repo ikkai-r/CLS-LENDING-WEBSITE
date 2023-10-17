e5939466d17e3cf435a9b5b844d562da172a2cfb
ikkai-r <rm.sales92@gmail.com>
2023-10-17 19:59:44 +0800

    modified:   public/css/style.css 	modified:   public/js/file_preview.js 	modified:   views/register.hbs

--- public/js/file_preview.js
+++ public/js/file_preview.js
@@ -1,22 +1,42 @@
-const inpFile = document.getElementById("inpFile");
-const previewContainer = document.getElementById("imagePreview");
-const previewImage = previewContainer.querySelector(".image-preview__image");
-const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+document.addEventListener("DOMContentLoaded", function() {
+    const inpFile = document.getElementById("id_image");
+    const previewImage = document.getElementById("image-preview__image-id");
 
-inpFile.addEventListener("change", function() {
-    const file = this.isDefaultNamespace[0];
+    const inpFile1 = document.getElementById("signature_image");
+    const previewImage1 = document.getElementById("image-preview__image-sig");
 
-    if (file) {
-        const reader = new FileReader();
+        inpFile.addEventListener("change", function() {
+            const file = inpFile.files[0];
 
-        previewDefaultText.style.display = "none";
-        previewImage.style.display = "block";
+            if (file) {
+                const reader = new FileReader();
 
-        reader.addEventListener("load", function() {
-            console.log(this);
-            previewImage.setAttribute("src", this.result);
+                previewImage.style.display = "block";
+
+                reader.addEventListener("load", function() {
+                    console.log(this);
+                    previewImage.setAttribute("src", this.result);
+                });
+
+                reader.readAsDataURL(file);
+            }
         });
 
-        reader.readAsDataURL(file);
-    }
+        inpFile1.addEventListener("change", function() {
+            const file = inpFile1.files[0];
+
+            if (file) {
+                const reader = new FileReader();
+
+                previewImage1.style.display = "block";
+
+                reader.addEventListener("load", function() {
+                    console.log(this);
+                    previewImage1.setAttribute("src", this.result);
+                });
+
+                reader.readAsDataURL(file);
+            }
+        });
+     
 });
