b5818bb46f8697117fc1b912a7c626ca4b20cc26
ikkai-r <rm.sales92@gmail.com>
2023-10-18 17:36:49 +0800

    added register v2

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,19 +2,16 @@ const controller = {
 
     getIndex: function(req, res) {
         // your code here
-        res.render('index', {header: 'test', layout: 'layouts/main', css: '/css/login.css'}); // This is to load the page initially
+        res.render('login', {header: 'test', layout: 'layouts/main_no', css: '/css/login2.css'}); // This is to load the page initially
     }, 
     getRegister: function(req, res) {
-        res.render('register', {layout: 'layouts/main', css: '/css/register.css'})
+        res.render('register', {layout: 'layouts/main_no', css: '/css/register.css'})
     },
     getDashboard: function (req, res) {
         res.render('dashboard', {layout: 'layouts/main'});
     },
     getClientDashboard: function (req, res) {
         res.render('client_dashboard', {layout: 'layouts/main_no'});
-    },
-    getLogin: function (req, res) {
-        res.render('login', {layout: 'layouts/main_no', css: '/css/login2.css'});
     }
 
 
--- public/js/file_preview.js
+++ public/js/file_preview.js
@@ -40,3 +40,102 @@ document.addEventListener("DOMContentLoaded", function() {
         });
      
 });
+
+var clicked = false;
+
+var my_handlers = {
+
+    fill_provinces:  function(){
+
+        var region_code = $(this).val();
+        $('#province').ph_locations('fetch_list', [{"region_code": region_code}]);
+        
+    },
+
+    fill_cities: function(){
+
+        var province_code = $(this).val();
+        $('#city').ph_locations( 'fetch_list', [{"province_code": province_code}]);
+    },
+
+
+    fill_barangays: function(){
+
+        var city_code = $(this).val();
+        $('#barangay').ph_locations('fetch_list', [{"city_code": city_code}]);
+    }
+};
+
+$(function () {
+    var clicked = false; 
+
+    $('#region').on('change', my_handlers.fill_provinces);
+    $('#province').on('change', my_handlers.fill_cities);
+    $('#city').on('change', my_handlers.fill_barangays);
+
+    $('#region').ph_locations({ 'location_type': 'regions' });
+    $('#province').ph_locations({ 'location_type': 'provinces' });
+    $('#city').ph_locations({ 'location_type': 'cities' });
+    $('#barangay').ph_locations({ 'location_type': 'barangays' });
+
+    $("#region").on("click", function () {
+        if (!clicked) {
+            $('#region').ph_locations('fetch_list');
+            clicked = true; 
+        }
+    });
+});
+
+$(function () {
+    // Initially, personal-info tab is active
+    // So, hide personal-info button and upload-info button, and change the text for contact-info button
+    $('#submit-btn').hide();
+    hideButtonsPersonal();
+    
+    // When the user clicks the "Contact Info" tab, show the personal-info and upload-info buttons
+    // and revert the text for the contact-info button
+    $('button[data-tabs-target="#contact-info"]').on('click', function () {
+        showButtonsContact();
+    });
+
+    $('button[data-tabs-target="#personal-info"]').on('click', function () {
+        hideButtonsPersonal();
+    });
+
+    $('button[data-tabs-target="#upload-info"]').on('click', function () {
+        showButtonsUpload();
+    });
+    
+    function hideButtonsPersonal() {
+        $('button[data-tabs-target="#personal-info"]').hide();
+        $('button[data-tabs-target="#upload-info"]').hide();
+        $('button[data-tabs-target="#contact-info"]').show();
+        $('button[data-tabs-target="#contact-info"]').text("Next Step");
+        $('#submit-btn').hide();
+    }
+    
+    function showButtonsContact() {
+        $('button[data-tabs-target="#personal-info"]').show();
+        $('button[data-tabs-target="#upload-info"]').show();
+        $('button[data-tabs-target="#contact-info"]').hide();
+        $('#submit-btn').hide();
+        $('button[data-tabs-target="#personal-info"]').text("Previous Step");
+        $('button[data-tabs-target="#upload-info"]').text("Next Step");
+    }
+
+    function showButtonsUpload() {
+        $('button[data-tabs-target="#personal-info"]').hide();
+        $('button[data-tabs-target="#upload-info"]').hide();
+        $('button[data-tabs-target="#contact-info"]').show();
+        $('button[data-tabs-target="#contact-info"]').text("Previous Step");
+        $('#submit-btn').show();
+    }
+});
+
+
+function submitAllForms() {
+    document.getElementById('personal-info-form').submit();
+    document.getElementById('contact-info-form').submit();
+    document.getElementById('upload-info-form').submit();
+}
+
