2af2da6b453087442ec5b3104f1a91d75c6f542d
koko-tsuu <colleen_tan@dlsu.edu.ph>
2023-10-24 15:19:25 +0800

    semi-fixed register? need help on how to input region

--- public/js/register.js
+++ public/js/register.js
@@ -14,13 +14,14 @@ function submitAllForms(){
             selected_gender = gender[i].value;
     }
     formData.append('gender', selected_gender);
-    formData.append('region', $('#region').val());
+    formData.append('region', $('#region').val()[0]);
     formData.append('province', $('#province').val());
     formData.append('city', $('#city').val());
     formData.append('barangay', $('#barangay').val());
     formData.append('region', $('#region').val());
     formData.append('add_details', $('#add_details').val());
     formData.append('email', $('#email').val());
+    formData.append('password', $('#password-tks').val());
     formData.append('contact_number', $('#contact_number').val());
     formData.append('facebook_link', $('#facebook_link').val());
 
