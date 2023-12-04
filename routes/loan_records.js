1d1a54425ea8bf8fcb4b443d07be94be6b6f3b96
koko-tsuu <colleen_tan@dlsu.edu.ph>
2023-12-04 08:28:21 +0800

    backend display loans

--- routes/loan_records.js
+++ routes/loan_records.js
@@ -4,18 +4,4 @@ const mongoose = require('mongoose');
 const Loan = require('../server/schema/Loan')
 
 const router = express.Router();
-
-router.get("/", async function(req, res) {
-
-
-    try {
-        const getLoans = await Loan.find().sort({"date":-1}).lean()
-        res.render("loan_records", {
-           loan: getLoans
-        });
-    
-    } catch(err) {
-        console.log(err);
-    }
-});
 module.exports = router;
